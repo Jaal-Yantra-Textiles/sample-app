1ee6193f6aa701be41ea91b42807ef2bd9edd589
Saransh Sharma <89117846+saranshisatgit@users.noreply.github.com>
2024-01-14 10:30:12 +0100

    Update saleor-app.ts

--- src/saleor-app.ts
+++ src/saleor-app.ts
@@ -14,6 +14,22 @@ switch (process.env.APL) {
     // Require `UPSTASH_URL` and `UPSTASH_TOKEN` environment variables
     apl = new UpstashAPL();
     break;
+  case "env":
+    const apl = new EnvAPL({
+          env: {
+            /**
+             * Map your env variables here. You don't have these values yet
+             */
+            token: envVars.SALEOR_APP_TOKEN,
+            appId: envVars.SALEOR_APP_ID,
+            saleorApiUrl: envVars.SALEOR_API_URL,
+          },
+          /**
+           * Set it to "true" - check your app logs during app registration. APL will print the values you need
+           */
+          printAuthDataOnRegister: true,
+        });
+    break;
   default:
     apl = new FileAPL();
 }
